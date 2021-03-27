504600521666d0a4b8c3e5a541d05ed8d556810b
Mickael Malolepsy <mickael@oh-my-app.com>
2021-03-28 00:24:42 +0100

    feat(security) require a token to create/update a badge

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,26 +17,26 @@ export const getBadge = functions.https.onRequest(async (request, response) => {
 	let badgeId: string = request.query.badge as string;
 	
 	if (!projectId || !badgeId) {
-		response.send('Missing parameters. Expected: project, badge.');
+		return response.end('Missing parameters. Expected: project, badge.');
 	}
 
 	// Check if the project exists
 	const projectRef = firestore.collection('projects').doc(projectId);
 	const project = await projectRef.get();
 	if (!project.exists) {
-	  response.send('No such project! You must create it in Firestore.');
+	  return response.end('No such project! You must create it in Firestore.');
 	}
 
 	// Create or update the badge
 	const badgeRef = projectRef.collection('badges').doc(badgeId);
 	const badge = await badgeRef.get();
 	if (!badge.exists) {
-	  response.send('No such badge!');
+	  return response.end('No such badge!');
 	}
 
 	const badgeData = badge.data();
 	if(!badgeData) {
-		response.send('No data for this badge!');
+		return response.end('No data for this badge!');
 	}
 
 	const label = badgeData!.label ?? "no-label" as string;
@@ -47,23 +47,34 @@ export const getBadge = functions.https.onRequest(async (request, response) => {
 });
 
 // Create or update a badge
-// Expected GET parameters: project, badge, label, color.
+// Expected GET parameters: project, badge, label, color, token.
 export const setBadge = functions.https.onRequest(async (request, response) => {
 	// Check required parameters
 	let projectId: string = request.query.project as string;
 	let badgeId: string = request.query.badge as string;
 	let label: string = request.query.label as string;
 	let color: string = request.query.color as string;
+	let token: string = request.query.token as string;
 
-	if (!projectId || !badgeId || !label || !color) {
-		response.send('Missing parameters. Expected: project, badge, label, color.');
+	if (!projectId || !badgeId || !label || !color || !token) {
+		return response.end('Missing parameters. Expected: project, badge, label, color, token.');
 	}
 
 	// Check if the project exists
 	const projectRef = firestore.collection('projects').doc(projectId);
 	const project = await projectRef.get();
 	if (!project.exists) {
-	  response.send('No such project! You must create it in Firestore.');
+	  return response.end('No such project! You must create it in Firestore.');
+	}
+
+	// Check token
+	const projectData = project.data();
+	if(!projectData) {
+		return response.end('The project is empty!');
+	}
+	const projectToken = projectData!.token ?? "" as string;
+	if(projectToken != token) {
+		return response.end('Wrong token goodbye!');
 	}
 
 	// Create or update the badge
